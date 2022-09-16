ab0da89c348bf6a4c231e5b879c552b04b8a062e
ugurcancaglayan <ugurcan.caglayan@nevalabs.com>
2022-09-17 01:34:40 +0300

    folder structure added

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../layouts/Footer'
+import Header from '../layouts/Header'
+
+function Login() {
+  return (
+    <div>
+        <Header />
+        <div>
+            Login orta sayfası
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default Login
