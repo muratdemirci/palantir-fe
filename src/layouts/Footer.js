5ef1ace80d05365a998d174d506f90e894fc9079
ugurcancaglayan <ugurcancaglayan@outlook.com>
2022-09-18 10:51:46 +0300

    footer updated

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
 		>
 
 			<footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-				<span class="text-sm  sm:text-center dark:text-gray-400">© 2022 Mordor İdman Yurdu™. All Rights Reserved.
+				<span class="text-sm  sm:text-center dark:text-gray-400">© Teknasyon Hackathon 22 - Yüzük Kardeşliği Hackathon, Our Team 2022 Mordor İdman Yurdu™. All Rights Reserved.
 				</span>
 				<ul class="flex flex-wrap items-center mt-3 text-sm dark:text-gray-400 sm:mt-0">
 					<li>
